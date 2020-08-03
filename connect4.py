0f29cd4264684cb43286b1d57e0c749f8fce0240
Alecu Cristian <alecucristianionut@gmail.com>
2020-08-03 16:59:51 +0300

    connect4 console functionaly

---
+++ connect4.py
@@ -0,0 +1,104 @@
+import numpy as np
+import math
+import pygame
+import sys
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+    return board
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0 :
+            return r
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    # Check vertical locations for win
+    for r in range(ROW_COUNT-3):
+        for c in range(COLUMN_COUNT):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # Check for positive sloped diagonals
+
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+
+    # Check for negative sloped diagonals
+
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+
+    
+
+
+
+
+
+board = create_board()
+game_over = False
+turn = 0
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+screen = pygame.display.set_mode(size)
+
+while not game_over:
+    # Ask for Player 1 Input
+    
+    print_board(board)
+    if turn == 0:
+        col = int(input("Player 1 Make your Selection (0-{}):".format(COLUMN_COUNT-1)))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+            if winning_move(board, 1):
+                print("Player 1 Wins!")
+                game_over = True
+
+    # Ask for Player 2 Input
+    else:
+        col = int(input("Player 2 Make your Selection (0-{}):".format(COLUMN_COUNT-1)))
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+            if winning_move(board, 2):
+                print("Player 2 Wins!")
+                game_over = True
+
+
+    turn += 1
+    turn %= 2
+
+    
